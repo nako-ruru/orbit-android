cb314b9ead4a453f9234eae9011b5457dd493ac4
Tao Wen <taowen@gmail.com>
2024-11-23 15:04:05 +0800

    fixui

--- app/src/main/java/com/gitee/connect_screen/LauncherActivity.java
+++ app/src/main/java/com/gitee/connect_screen/LauncherActivity.java
@@ -12,6 +12,10 @@ import android.view.WindowManager;
 import android.os.Build;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.content.res.Resources;
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+import java.util.stream.Collectors;
 
 public class LauncherActivity extends AppCompatActivity {
     @Override
@@ -35,12 +39,28 @@ public class LauncherActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.app_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         
-        // 获取已安装的应用
+        // 获取已安装的应用并过滤系统应用
         PackageManager pm = getPackageManager();
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
         
+        // 过滤掉系统应用
+        List<ApplicationInfo> userApps = packages.stream()
+            .filter(app -> (app.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
+            .collect(Collectors.toList());
+        
         // 创建适配器（需要另外实现）
-        AppListAdapter adapter = new AppListAdapter(packages, pm);
+        AppListAdapter adapter = new AppListAdapter(userApps, pm);
         recyclerView.setAdapter(adapter);
+        
+        // 添加设置 DPI 的代码
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        
+        displayMetrics.densityDpi = 320; 
+        configuration.densityDpi = 320;
+        
+        // 应用新的配置
+        resources.updateConfiguration(configuration, displayMetrics);
     }
 }
