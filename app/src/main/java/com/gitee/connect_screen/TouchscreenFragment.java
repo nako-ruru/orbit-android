9a6510878c1a7579e2272e16898f8c580ba15535
Tao Wen <taowen@gmail.com>
2024-12-07 14:32:43 +0800

    touchscreen

--- app/src/main/java/com/gitee/connect_screen/TouchscreenFragment.java
+++ app/src/main/java/com/gitee/connect_screen/TouchscreenFragment.java
@@ -11,8 +11,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TouchscreenFragment extends Fragment {
     private static final String ARG_HID_DESC = "hid_descriptor";
@@ -28,6 +32,104 @@ public class TouchscreenFragment extends Fragment {
     private UsbDeviceConnection connection;
     private UsbEndpoint inputEndpoint;
     private UsbManager usbManager;
+    
+    private void parseInputFormat() {
+        if (reportDescriptor == null) return;
+        
+        // 解析报告描述符以获取输入格式
+        TouchInputFormat format = new TouchInputFormat();
+        int i = 0;
+        int currentUsagePage = 0;
+        boolean inInputReport = false;
+        int currentReportSize = 0;
+        int currentReportCount = 0;
+        
+        while (i < reportDescriptor.length) {
+            int prefix = reportDescriptor[i] & 0xFF;
+            int tag = (prefix >> 4) & 0x0F;
+            int type = (prefix >> 2) & 0x03;
+            int size = prefix & 0x03;
+            
+            long data = 0;
+            if (size > 0 && i + size < reportDescriptor.length) {
+                for (int j = 0; j < size; j++) {
+                    data |= (reportDescriptor[i + 1 + j] & 0xFF) << (j * 8);
+                }
+            }
+
+            switch (type) {
+                case 0: // 主项
+                    if (tag == 8) { // Input
+                        inInputReport = true;
+                        format.addField(currentReportSize, currentReportCount);
+                    }
+                    break;
+                    
+                case 1: // 全局项
+                    switch (tag) {
+                        case 0: // Usage Page
+                            currentUsagePage = (int)data;
+                            break;
+                        case 7: // Report Size
+                            currentReportSize = (int)data;
+                            break;
+                        case 9: // Report Count
+                            currentReportCount = (int)data;
+                            break;
+                    }
+                    break;
+                    
+                case 2: // 局部项
+                    if (tag == 0) { // Usage
+                        if (currentUsagePage == 0x0D) { // Digitizer
+                            switch ((int)data) {
+                                case 0x30: // Tip Pressure
+                                    format.hasPressure = true;
+                                    break;
+                                case 0x32: // In Range
+                                    format.hasInRange = true;
+                                    break;
+                                case 0x42: // Tip Switch
+                                    format.hasTipSwitch = true;
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+            }
+            
+            i += 1 + size;
+        }
+        
+        // 保存解析结果
+        inputFormat = format;
+    }
+    
+    // 用于存储触摸输入格式的内部类
+    private static class TouchInputFormat {
+        List<FieldInfo> fields = new ArrayList<>();
+        boolean hasPressure = false;
+        boolean hasInRange = false;
+        boolean hasTipSwitch = false;
+        int totalBits = 0;
+        
+        void addField(int size, int count) {
+            fields.add(new FieldInfo(size, count));
+            totalBits += size * count;
+        }
+        
+        static class FieldInfo {
+            final int size;
+            final int count;
+            
+            FieldInfo(int size, int count) {
+                this.size = size;
+                this.count = count;
+            }
+        }
+    }
+    
+    private TouchInputFormat inputFormat;
     
     public static TouchscreenFragment newInstance(byte[] hidDesc, byte[] reportDesc, 
             UsbDevice device, int interfaceIndex) {
@@ -62,6 +164,9 @@ public class TouchscreenFragment extends Fragment {
                     }
                 }
             }
+            
+            // 解析输入格式
+            parseInputFormat();
         }
         usbManager = (UsbManager) requireContext().getSystemService(Context.USB_SERVICE);
     }
@@ -69,7 +174,34 @@ public class TouchscreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_touchscreen, container, false);
-        // TODO: 实现触摸屏界面
+        
+        TextView formatInfo = view.findViewById(R.id.formatInfo);
+        displayFormatInfo(formatInfo);
+        
         return view;
+    }
+    
+    private void displayFormatInfo(TextView textView) {
+        if (inputFormat == null) {
+            textView.setText("未能解析输入格式");
+            return;
+        }
+        
+        StringBuilder info = new StringBuilder();
+        info.append("触摸输入格式信息：\n\n");
+        
+        info.append("支持的功能：\n");
+        info.append("- 压力感应: ").append(inputFormat.hasPressure ? "是" : "否").append("\n");
+        info.append("- 悬空检测: ").append(inputFormat.hasInRange ? "是" : "否").append("\n");
+        info.append("- 接触检测: ").append(inputFormat.hasTipSwitch ? "是" : "否").append("\n\n");
+        
+        info.append("数据字段：\n");
+        for (int i = 0; i < inputFormat.fields.size(); i++) {
+            TouchInputFormat.FieldInfo field = inputFormat.fields.get(i);
+            info.append(String.format("字段 %d: %d位 x %d个\n", i + 1, field.size, field.count));
+        }
+        info.append("\n总位数：").append(inputFormat.totalBits).append("位");
+        
+        textView.setText(info.toString());
     }
 } 
