35bd976ecfebcdc12c49fa74766dbb84b4128fbb
Tao Wen <taowen@gmail.com>
2024-12-08 10:02:12 +0800

    v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gitee.connect_screen"
         minSdk = 30
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.1.2"
+        versionCode = 13
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
