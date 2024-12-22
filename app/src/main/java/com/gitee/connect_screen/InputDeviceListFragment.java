3687a948c434a38cb88a853a6c28ba30164b95dd
Tao Wen <taowen@gmail.com>
2024-12-22 21:23:21 +0800

    update

--- app/src/main/java/com/gitee/connect_screen/InputDeviceListFragment.java
+++ app/src/main/java/com/gitee/connect_screen/InputDeviceListFragment.java
@@ -1,6 +1,8 @@
 package com.gitee.connect_screen;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.InputDevice;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.hardware.input.InputManager;
+
 public class InputDeviceListFragment extends Fragment {
 
     @Nullable
@@ -18,7 +22,30 @@ public class InputDeviceListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_input_device_list, container, false);
         
         TextView placeholderText = view.findViewById(R.id.placeholderText);
-        placeholderText.setText("输入设备列表");
+        
+        // 获取输入设备管理器
+        InputManager inputManager = (InputManager) requireContext().getSystemService(Context.INPUT_SERVICE);
+        
+        // 获取所有输入设备ID
+        int[] deviceIds = inputManager.getInputDeviceIds();
+        
+        // 构建设备列表字符串
+        StringBuilder deviceList = new StringBuilder();
+        deviceList.append("外接输入设备列表:\n\n");
+        
+        for (int deviceId : deviceIds) {
+            InputDevice device = InputDevice.getDevice(deviceId);
+            if (device != null && device.isExternal()) {
+                deviceList.append(device.getName()).append("\n");
+            }
+        }
+        
+        // 如果没有外接设备，显示提示信息
+        if (deviceList.toString().equals("外接输入设备列表:\n\n")) {
+            deviceList.append("未检测到外接输入设备");
+        }
+        
+        placeholderText.setText(deviceList.toString());
         
         return view;
     }
