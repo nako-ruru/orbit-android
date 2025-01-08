de786b6b100f315ff37bc93efda5112f3c25d9be
Tao Wen <taowen@gmail.com>
2025-01-08 22:23:03 +0800

    update

--- app/src/main/java/com/gitee/connect_screen/BridgeActivity.java
+++ app/src/main/java/com/gitee/connect_screen/BridgeActivity.java
@@ -6,10 +6,6 @@ import android.graphics.Color;
 import android.graphics.SurfaceTexture;
 import android.hardware.display.VirtualDisplay;
 import android.media.ImageReader;
-import android.opengl.GLES11Ext;
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,6 +16,9 @@ import android.view.Surface;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.TextureView;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,9 +28,6 @@ import com.gitee.connect_screen.shizuku.ServiceUtils;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
 import dev.rikka.tools.refine.Refine;
 
@@ -43,9 +39,7 @@ public class BridgeActivity extends AppCompatActivity {
         return instance;
     }
 
-    private GLSurfaceView glSurfaceView;
-    private OpenGLRenderer renderer;
-
+    private SurfaceView surfaceView;
 
     // 添加坐标调整方法
     private static float[] adjustTouchCoordinates(float x, float y, int rotation,
@@ -121,16 +115,44 @@ public class BridgeActivity extends AppCompatActivity {
         window.setStatusBarColor(Color.TRANSPARENT);
         window.setNavigationBarColor(Color.TRANSPARENT);
 
-        // 创建并设置 GLSurfaceView
-        glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setEGLContextClientVersion(2);
+        // 创建并设置 SurfaceView
+        surfaceView = new SurfaceView(this);
         VirtualDisplayArgs args = getIntent().getParcelableExtra("virtualDisplayArgs");
-        renderer = new OpenGLRenderer(glSurfaceView, args);
-        glSurfaceView.setRenderer(renderer);
-        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Surface surface = holder.getSurface();
+                
+                if (State.bridgeVirtualDisplay == null) {
+                    stopVirtualDisplay();
+                    State.bridgeVirtualDisplay = CreateVirtualDisplay.createVirtualDisplay(args, surface);
+                    State.log("Bridge Activity 创建了新的虚拟显示器");
+                } else {
+                    State.bridgeVirtualDisplay.setSurface(surface);
+                    State.log("Bridge Activity 复用了已有的虚拟显示器");
+                }
+                
+                State.breadcrumbManager.popBreadcrumb();
+                Display jumpToDisplay = State.bridgeVirtualDisplay.getDisplay();
+                State.breadcrumbManager.pushBreadcrumb("屏幕 " + jumpToDisplay.getDisplayId(), 
+                    () -> DisplayDetailFragment.newInstance(jumpToDisplay.getDisplayId()));
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                if (State.bridgeVirtualDisplay != null) {
+                    ImageReader imageReader = ImageReader.newInstance(args.monitorWidth, args.monitorHeight, 1, 2);
+                    State.bridgeVirtualDisplay.setSurface(imageReader.getSurface());
+                }
+            }
+        });
         
         // 添加触摸事件监听
-        glSurfaceView.setOnTouchListener((v, event) -> {
+        surfaceView.setOnTouchListener((v, event) -> {
             if (State.bridgeVirtualDisplay != null) {
                 // 获取显示ID和旋转角度
                 Display virtualDisplay = State.bridgeVirtualDisplay.getDisplay();
@@ -144,7 +166,7 @@ public class BridgeActivity extends AppCompatActivity {
                 // 根据旋转角度调整坐标
                 float[] adjustedCoords = adjustTouchCoordinates(x, y, rotation, 
                     args.monitorWidth, args.monitorHeight, 
-                    glSurfaceView.getWidth(), glSurfaceView.getHeight());
+                    surfaceView.getWidth(), surfaceView.getHeight());
                 
                 // 设置调整后的坐标
                 event.setLocation(adjustedCoords[0], adjustedCoords[1]);
@@ -161,16 +183,12 @@ public class BridgeActivity extends AppCompatActivity {
             return true;
         });
         
-        setContentView(glSurfaceView);
+        setContentView(surfaceView);
     }
 
     @Override
     protected void onDestroy() {
         Log.i("BridgeActivity", "BridgeActivity onDestroy");
-        if (renderer != null) {
-            renderer.release();
-            renderer = null;
-        }
         super.onDestroy();
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -178,219 +196,5 @@ public class BridgeActivity extends AppCompatActivity {
         options.setLaunchDisplayId(0);
         this.startActivity(intent, options.toBundle());
         instance = null;
-    }
-
-    private static class OpenGLRenderer implements GLSurfaceView.Renderer {
-        private final float[] mvpMatrix = new float[16];
-        private int program;
-        private int positionHandle;
-        private int texcoordHandle;
-        private int textureHandle;
-        private int mvpMatrixHandle;
-        private ByteBuffer vertexBuffer;
-        private ByteBuffer texcoordBuffer;
-        private final GLSurfaceView glSurfaceView;
-        private final VirtualDisplayArgs args;
-        private int textureId;
-        private Surface surface;
-        private SurfaceTexture surfaceTexture;
-
-        private final String vertexShader = "attribute vec4 position;\n" +
-                "attribute vec2 texcoord;\n" +
-                "varying vec2 v_texcoord;\n" +
-                "uniform mat4 uMVPMatrix;\n" +
-                "void main() {\n" +
-                "    gl_Position = uMVPMatrix * position;\n" +
-                "    v_texcoord = texcoord;\n" +
-                "}";
-
-        private final String fragmentShader = "#extension GL_OES_EGL_image_external : require\n" +
-                "precision mediump float;\n" +
-                "varying vec2 v_texcoord;\n" +
-                "uniform samplerExternalOES texture;\n" +
-                "void main() {\n" +
-                "    gl_FragColor = texture2D(texture, v_texcoord);\n" +
-                "}";
-
-        public OpenGLRenderer(GLSurfaceView glSurfaceView, VirtualDisplayArgs args) {
-            this.glSurfaceView = glSurfaceView;
-            this.args = args;
-        }
-
-        @Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            // 初始化纹理
-            int[] textures = new int[1];
-            GLES20.glGenTextures(1, textures, 0);
-            textureId = textures[0];
-            // 创建 SurfaceTexture 和 Surface
-            surfaceTexture = new SurfaceTexture(textureId);
-            surfaceTexture.setDefaultBufferSize(args.monitorWidth, args.monitorHeight);
-            surface = new Surface(surfaceTexture);
-            surfaceTexture.setOnFrameAvailableListener(surfaceTexture -> {
-                // 请求重新绘制
-                glSurfaceView.requestRender();
-            });
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureId);
-            GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-
-            // 初始化顶点和纹理坐标
-            float[] vertices = {
-                    -1.0f, -1.0f, // 左下
-                    1.0f, -1.0f, // 右下
-                    -1.0f, 1.0f, // 左上
-                    1.0f, 1.0f // 右上
-            };
-
-            float[] texCoords = {
-                    0.0f, 1.0f, // 左下
-                    1.0f, 1.0f, // 右下
-                    0.0f, 0.0f, // 左上
-                    1.0f, 0.0f  // 右上
-            };
-
-            vertexBuffer = ByteBuffer.allocateDirect(vertices.length * 4)
-                    .order(ByteOrder.nativeOrder());
-            vertexBuffer.asFloatBuffer().put(vertices).position(0);
-
-            texcoordBuffer = ByteBuffer.allocateDirect(texCoords.length * 4)
-                    .order(ByteOrder.nativeOrder());
-            texcoordBuffer.asFloatBuffer().put(texCoords).position(0);
-
-            // 初始化着色器程序
-            program = createProgram();
-
-            
-            GLES20.glUseProgram(program);
-
-            // 启用顶点属性数组
-            GLES20.glEnableVertexAttribArray(positionHandle);
-            GLES20.glEnableVertexAttribArray(texcoordHandle);
-
-            // 设置顶点坐标数据
-            GLES20.glVertexAttribPointer(positionHandle, 2, GLES20.GL_FLOAT, false, 0, vertexBuffer);
-            // 设置纹理坐标数据
-            GLES20.glVertexAttribPointer(texcoordHandle, 2, GLES20.GL_FLOAT, false, 0, texcoordBuffer);
-
-            // 设置MVP矩阵
-            Matrix.setIdentityM(mvpMatrix, 0);
-            GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0);
-
-            // 绑定纹理
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureId);
-            GLES20.glUniform1i(textureHandle, 0);
-            instance.runOnUiThread(() -> {
-                startProjection();
-            });
-        }
-
-        private void startProjection() {
-            if (State.bridgeVirtualDisplay == null) {
-                // 使用传入的参数创建虚拟显示
-                stopVirtualDisplay();
-                State.bridgeVirtualDisplay = CreateVirtualDisplay.createVirtualDisplay(args, surface);
-                State.log("Bridge Activity 创建了新的虚拟显示器");
-            } else {
-                State.bridgeVirtualDisplay.setSurface(surface);
-                State.log("Bridge Activity 复用了已有的虚拟显示器");
-            }
-            State.breadcrumbManager.popBreadcrumb();
-            Display jumpToDisplay = State.bridgeVirtualDisplay.getDisplay();
-            State.breadcrumbManager.pushBreadcrumb("屏幕 " + jumpToDisplay.getDisplayId(), () -> DisplayDetailFragment.newInstance(jumpToDisplay.getDisplayId()));
-        }
-
-        @Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-            GLES20.glViewport(0, 0, width, height);
-        }
-
-        @Override
-        public void onDrawFrame(GL10 gl) {
-            if (State.bridgeVirtualDisplay == null) {
-                return;
-            }
-            try {
-                surfaceTexture.updateTexImage();
-                // 绘制
-                GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-            } catch (Throwable e) {
-                Log.e("BridgeActivity", "Failed to draw frame", e);
-            }
-        }
-
-        private int createProgram() {
-            // 编译顶点着色器
-            int vertexShaderHandle = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
-            GLES20.glShaderSource(vertexShaderHandle, vertexShader);
-            GLES20.glCompileShader(vertexShaderHandle);
-
-            // 检查编译状态
-            int[] status = new int[1];
-            GLES20.glGetShaderiv(vertexShaderHandle, GLES20.GL_COMPILE_STATUS, status, 0);
-            if (status[0] == 0) {
-                return 0;
-            }
-
-            // 编译片段着色器
-            int fragmentShaderHandle = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
-            GLES20.glShaderSource(fragmentShaderHandle, fragmentShader);
-            GLES20.glCompileShader(fragmentShaderHandle);
-
-            GLES20.glGetShaderiv(fragmentShaderHandle, GLES20.GL_COMPILE_STATUS, status, 0);
-            if (status[0] == 0) {
-                return 0;
-            }
-
-            // 创建程序并链接着色器
-            int programHandle = GLES20.glCreateProgram();
-            GLES20.glAttachShader(programHandle, vertexShaderHandle);
-            GLES20.glAttachShader(programHandle, fragmentShaderHandle);
-            GLES20.glLinkProgram(programHandle);
-
-            GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, status, 0);
-            if (status[0] == 0) {
-                return 0;
-            }
-
-            // 删除着色器
-            GLES20.glDeleteShader(vertexShaderHandle);
-            GLES20.glDeleteShader(fragmentShaderHandle);
-
-            // 获取属性和uniform位置
-            positionHandle = GLES20.glGetAttribLocation(programHandle, "position");
-            texcoordHandle = GLES20.glGetAttribLocation(programHandle, "texcoord");
-            textureHandle = GLES20.glGetUniformLocation(programHandle, "texture");
-            mvpMatrixHandle = GLES20.glGetUniformLocation(programHandle, "uMVPMatrix");
-
-            return programHandle;
-        }
-
-        public void release() {
-            ImageReader imageReader = ImageReader.newInstance(args.monitorWidth, args.monitorHeight, 1, 2);
-            if (State.bridgeVirtualDisplay != null) {
-                State.bridgeVirtualDisplay.setSurface(imageReader.getSurface());
-            }
-            if (surface != null) {
-                surface.release();
-                surface = null;
-            }
-            if (surfaceTexture != null) {
-                surfaceTexture.setOnFrameAvailableListener(null);
-                surfaceTexture.release();
-                surfaceTexture = null;
-            }
-            if (textureId != 0) {
-                int[] textures = {textureId};
-                GLES20.glDeleteTextures(1, textures, 0);
-                textureId = 0;
-            }
-            if (program != 0) {
-                GLES20.glDeleteProgram(program);
-                program = 0;
-            }
-        }
-
     }
 }
