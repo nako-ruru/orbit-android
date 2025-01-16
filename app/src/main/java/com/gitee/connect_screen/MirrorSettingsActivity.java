06573c134a68c207a799f2668ba5baf99efbfd0a
Tao Wen <taowen@gmail.com>
2025-01-16 21:05:04 +0800

    rename

--- app/src/main/java/com/gitee/connect_screen/MirrorSettingsActivity.java
+++ app/src/main/java/com/gitee/connect_screen/MirrorSettingsActivity.java
@@ -3,10 +3,10 @@ package com.gitee.connect_screen;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MirrorActivity extends AppCompatActivity {
+public class MirrorSettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mirror);
+        setContentView(R.layout.activity_mirror_settings);
     }
 } 
