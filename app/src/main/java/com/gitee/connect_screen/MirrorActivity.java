16b991fbe3d818518df14d9465731fee451ce809
Tao Wen <taowen@gmail.com>
2025-01-16 21:03:18 +0800

    add new icon

---
+++ app/src/main/java/com/gitee/connect_screen/MirrorActivity.java
@@ -0,0 +1,12 @@
+package com.gitee.connect_screen;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MirrorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mirror);
+    }
+} 
