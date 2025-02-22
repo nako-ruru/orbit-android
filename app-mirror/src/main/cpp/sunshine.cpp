0305b4134ee975a7133c79ff6d1237bcf1b86895
Tao Wen <taowen@gmail.com>
2025-02-22 22:22:47 +0800

    add boost

--- app-mirror/src/main/cpp/sunshine.cpp
+++ app-mirror/src/main/cpp/sunshine.cpp
@@ -1,11 +1,17 @@
 #include <jni.h>
 #include <string>
+#include "logging.h"
+
+using namespace std::literals;
 
 extern "C" {
 
+static std::unique_ptr<logging::deinit_t> deinit;
+
 JNIEXPORT void JNICALL
 Java_com_connect_1screen_mirror_job_SunshineServer_start(JNIEnv *env, jobject thiz) {
-    // TODO: 在这里实现你的服务器启动逻辑
+    deinit = logging::init(0, "/dev/null");
+    BOOST_LOG(info) << "start sunshine server"sv;
 }
 
 } 
--- app-mirror/src/main/java/com/connect_screen/mirror/job/SunshineServer.java
+++ app-mirror/src/main/java/com/connect_screen/mirror/job/SunshineServer.java
@@ -1,5 +1,6 @@
 package com.connect_screen.mirror.job;
 
+// 代码拷贝自 v2025.122.141614
 public class SunshineServer {
     static {
         System.loadLibrary("sunshine");
