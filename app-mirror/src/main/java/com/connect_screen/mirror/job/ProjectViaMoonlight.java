74a8a7ca528debae0d6ccacad1fc01f5b5b5088a
Tao Wen <taowen@gmail.com>
2025-03-02 12:05:57 +0800

    update

--- app-mirror/src/main/java/com/connect_screen/mirror/job/ProjectViaMoonlight.java
+++ app-mirror/src/main/java/com/connect_screen/mirror/job/ProjectViaMoonlight.java
@@ -1,11 +1,21 @@
 package com.connect_screen.mirror.job;
 
+import static com.connect_screen.mirror.MirrorMainActivity.REQUEST_RECORD_AUDIO_PERMISSION;
+
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioPlaybackCaptureConfiguration;
+import android.media.AudioRecord;
 import android.media.projection.MediaProjectionConfig;
 import android.media.projection.MediaProjectionManager;
 import android.view.Surface;
 
+
+import androidx.core.app.ActivityCompat;
 
 import com.connect_screen.mirror.MediaProjectionService;
 import com.connect_screen.mirror.MirrorMainActivity;
@@ -18,6 +28,7 @@ public class ProjectViaMoonlight implements Job {
     private final int frameRate;
     private final Surface surface;
     private boolean mediaProjectionRequested;
+    private boolean audioPermissionRequested;
 
     public ProjectViaMoonlight(int width, int height, int frameRate, Surface surface) {
         this.width = width;
@@ -28,11 +39,28 @@ public class ProjectViaMoonlight implements Job {
 
     @Override
     public void start() throws YieldException {
-        if (requestMediaProjectionPermission(State.currentActivity.get())) {
-            SunshineServer.autoRotateAndScaleForMoonlight = new AutoRotateAndScaleForMoonlight(new VirtualDisplayArgs("ScreenCapture",
-                    width, height, frameRate, 160, false));
-            SunshineServer.autoRotateAndScaleForMoonlight.start(surface);
+        if (!requestMediaProjectionPermission(State.currentActivity.get())) {
+            return;
         }
+        // 创建AudioRecord
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            if (ActivityCompat.checkSelfPermission(State.currentActivity.get(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                new AudioRecordingThread(State.currentActivity.get(), State.getMediaProjection()).start();
+            } else {
+                if (audioPermissionRequested) {
+                    State.log("因为未授予录音权限，跳过任务");
+                    return;
+                }
+                audioPermissionRequested = true;
+                ActivityCompat.requestPermissions(State.currentActivity.get(),
+                        new String[]{Manifest.permission.RECORD_AUDIO}, 
+                        REQUEST_RECORD_AUDIO_PERMISSION);
+                throw new YieldException("等待录音权限授权");
+            }
+        }
+        // SunshineServer.autoRotateAndScaleForMoonlight = new AutoRotateAndScaleForMoonlight(new VirtualDisplayArgs("ScreenCapture",
+        //         width, height, frameRate, 160, false));
+        // SunshineServer.autoRotateAndScaleForMoonlight.start(surface);
     }
 
     private boolean requestMediaProjectionPermission(Context context) throws YieldException {
