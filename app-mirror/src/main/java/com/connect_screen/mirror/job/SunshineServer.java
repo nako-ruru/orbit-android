f16a59d422ad76ac16c7fc6882502f3f41882d41
Tao Wen <taowen@gmail.com>
2025-03-03 22:23:28 +0800

    update

--- app-mirror/src/main/java/com/connect_screen/mirror/job/SunshineServer.java
+++ app-mirror/src/main/java/com/connect_screen/mirror/job/SunshineServer.java
@@ -21,7 +21,9 @@ import com.connect_screen.mirror.shizuku.ServiceUtils;
 import com.connect_screen.mirror.shizuku.ShizukuUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import dev.rikka.tools.refine.Refine;
 
@@ -125,12 +127,11 @@ public class SunshineServer {
     }
 
     private static class PointerStatus {
-        public boolean isDown = false;
         public float x = 0;
         public float y = 0;
     }
 
-    private static List<PointerStatus> pointers = new ArrayList<>();
+    private static Map<Integer, PointerStatus> pointers = new HashMap<>();
 
     // 添加处理触摸事件的静态方法
     public static void handleTouchPacket(int eventType, int rotation, int pointerId, 
@@ -139,18 +140,37 @@ public class SunshineServer {
         if (inputManager == null) {
             return;
         }
+        int displayRotation = State.mirrorVirtualDisplay.getDisplay().getRotation();
+        // 根据屏幕旋转调整坐标
+        float adjustedX = x * screenWidth;
+        float adjustedY = y * screenHeight;
+        switch (displayRotation) {
+            case Surface.ROTATION_90:
+                adjustedX = (1 - y) * screenWidth;
+                adjustedY = x * screenHeight;
+                break;
+            case Surface.ROTATION_180:
+                adjustedX = (1 - x) * screenWidth;
+                adjustedY = (1 - y) * screenHeight;
+                break;
+            case Surface.ROTATION_270:
+                adjustedX = y * screenWidth;
+                adjustedY = (1 - x) * screenHeight;
+                break;
+        }
+        pointerId = pointerId % 10;
         switch (eventType) {
             case 0x01: // LI_TOUCH_EVENT_DOWN
-                handleTouchEventDown(pointerId, x, y);
+                handleTouchEventDown(pointerId, adjustedX, adjustedY);
                 break;
             case 0x02: // LI_TOUCH_EVENT_UP
-                handleTouchEventUp(pointerId, x, y, false);
+                handleTouchEventUp(pointerId, adjustedX, adjustedY, false);
                 break;
             case 0x03: // LI_TOUCH_EVENT_MOVE
-                handleTouchEventMove(pointerId, x, y);
+                handleTouchEventMove(pointerId, adjustedX, adjustedY);
                 break;
             case 0x04: // LI_TOUCH_EVENT_CANCEL
-                handleTouchEventUp(pointerId, x, y, true);
+                handleTouchEventUp(pointerId, adjustedX, adjustedY, true);
                 break;
             case 0x07: // LI_TOUCH_EVENT_CANCEL_ALL
                 handleTouchEventCancelAll();
@@ -160,48 +180,36 @@ public class SunshineServer {
         }
     }
 
-    private static PointerStatus getPointerStatus(int pointerId) {
-        while (pointerId >= pointers.size()) {
-            pointers.add(new PointerStatus());
-        }
-        return pointers.get(pointerId);
-    }
-
     private static void handleTouchEventDown(int pointerId, float x, float y) {
-        PointerStatus pointerStatus = getPointerStatus(pointerId);
-        pointerStatus.isDown = true;
-        pointerStatus.x = screenWidth * x;
-        pointerStatus.y = screenHeight * y;
+        if (!pointers.containsKey(pointerId)) {
+            PointerStatus pointerStatus = new PointerStatus();
+            pointerStatus.x = x;
+            pointerStatus.y = y;
+            pointers.put(pointerId, pointerStatus);
+        }
 
-        int action = pointerId == 0 ? android.view.MotionEvent.ACTION_DOWN : 
+        int action = pointers.size() == 1 ? android.view.MotionEvent.ACTION_DOWN :
                                     android.view.MotionEvent.ACTION_POINTER_DOWN | (pointerId << 8);
         // 构造 MotionEvent
         long downTime = SystemClock.uptimeMillis();
         long eventTime = SystemClock.uptimeMillis();
         
-        // 创建包含所有活跃触摸点的属性数组
-        int pointerCount = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            if (pointers.get(i).isDown) pointerCount++;
-        }
-        
-        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
-        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
+
+        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointers.size()];
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointers.size()];
         
         int index = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            PointerStatus status = pointers.get(i);
-            if (status.isDown) {
-                properties[index] = new MotionEvent.PointerProperties();
-                properties[index].id = i;  // 保持id为原始的pointerId
-                properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
-                
-                coords[index] = new MotionEvent.PointerCoords();
-                coords[index].x = status.x;
-                coords[index].y = status.y;
-                coords[index].pressure = 1.0f;
-                index++;
-            }
+        for (Integer k : pointers.keySet()) {
+            PointerStatus status = pointers.get(k);
+            properties[index] = new MotionEvent.PointerProperties();
+            properties[index].id = k;  // 保持id为原始的pointerId
+            properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
+
+            coords[index] = new MotionEvent.PointerCoords();
+            coords[index].x = status.x;
+            coords[index].y = status.y;
+            coords[index].pressure = 1.0f;
+            index++;
         }
         
         // 构造 MotionEvent
@@ -209,7 +217,7 @@ public class SunshineServer {
             downTime,
             eventTime,
             action,
-            pointerCount, // 使用实际的触摸点数量
+            pointers.size(), // 使用实际的触摸点数量
             properties,
             coords,
             0, // metaState
@@ -225,51 +233,42 @@ public class SunshineServer {
             MotionEventHidden motionEventHidden = Refine.unsafeCast(event);
             motionEventHidden.setDisplayId(State.mirrorVirtualDisplay.getDisplay().getDisplayId());
             inputManager.injectInputEvent(event, 0);
+            android.util.Log.d("SunshineServer", "inject down: " + event);
         }
     }
 
     private static void handleTouchEventUp(int pointerId, float x, float y, boolean cancelled) {
-        // 更新指针状态
-        if (pointerId >= pointers.size() || !pointers.get(pointerId).isDown) {
+        PointerStatus status = pointers.get(pointerId);
+        if(status == null) {
             return;
         }
-        
-        PointerStatus status = pointers.get(pointerId);
-        status.isDown = false;
-        
+        status.x = x;
+        status.y = y;
+
         // 确定动作类型
-        int action = cancelled ? android.view.MotionEvent.ACTION_CANCEL :
-                    (pointerId == 0 ? android.view.MotionEvent.ACTION_UP :
-                    android.view.MotionEvent.ACTION_POINTER_UP | (pointerId << 8));
+        int action = pointers.size() == 1 ? android.view.MotionEvent.ACTION_UP :
+                android.view.MotionEvent.ACTION_POINTER_UP | (pointerId << 8);
 
         // 构造 MotionEvent
         long downTime = SystemClock.uptimeMillis();
         long eventTime = SystemClock.uptimeMillis();
 
-        // 计算活跃的触摸点数量
-        int pointerCount = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            if (pointers.get(i).isDown || i == pointerId) pointerCount++;
-        }
-
         // 创建包含所有活跃触摸点的属性数组
-        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
-        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
+        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointers.size()];
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointers.size()];
 
         int index = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            PointerStatus ps = pointers.get(i);
-            if (ps.isDown || i == pointerId) {
-                properties[index] = new MotionEvent.PointerProperties();
-                properties[index].id = i;  // 保持id为原始的pointerId
-                properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
+        for (Integer k : pointers.keySet()) {
+            PointerStatus ps = pointers.get(k);
+            properties[index] = new MotionEvent.PointerProperties();
+            properties[index].id = k;  // 保持id为原始的pointerId
+            properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
 
-                coords[index] = new MotionEvent.PointerCoords();
-                coords[index].x = ps.x;
-                coords[index].y = ps.y;
-                coords[index].pressure = i == pointerId ? 0.0f : 1.0f;
-                index++;
-            }
+            coords[index] = new MotionEvent.PointerCoords();
+            coords[index].x = ps.x;
+            coords[index].y = ps.y;
+            coords[index].pressure = k == pointerId ? 0.0f : 1.0f;
+            index++;
         }
 
         // 构造并注入 MotionEvent
@@ -277,7 +276,7 @@ public class SunshineServer {
             downTime,
             eventTime,
             action,
-            pointerCount, // 使用实际的触摸点数量
+            pointers.size(), // 使用实际的触摸点数量
             properties,
             coords,
             0, // metaState
@@ -287,26 +286,28 @@ public class SunshineServer {
             0, // deviceId
             0, // edgeFlags
             InputDevice.SOURCE_TOUCHSCREEN,
-            0 // flags
+            cancelled ? MotionEvent.FLAG_CANCELED : 0 // flags
         );
+
+        pointers.remove(pointerId);
 
         if (State.mirrorVirtualDisplay != null) {
             MotionEventHidden motionEventHidden = Refine.unsafeCast(event);
             motionEventHidden.setDisplayId(State.mirrorVirtualDisplay.getDisplay().getDisplayId());
             inputManager.injectInputEvent(event, 0);
+            android.util.Log.d("SunshineServer", "inject up: " + event);
         }
     }
 
     private static void handleTouchEventMove(int pointerId, float x, float y) {
-        // 添加移动事件处理逻辑
-        if (pointerId >= pointers.size() || !pointers.get(pointerId).isDown) {
+        PointerStatus status = pointers.get(pointerId);
+        if (status == null) {
             return;
         }
         
         // 更新指针位置
-        PointerStatus status = pointers.get(pointerId);
-        status.x = screenWidth * x;
-        status.y = screenHeight * y;
+        status.x = x;
+        status.y = y;
         
         triggerTouchEventMove();
     }
@@ -316,52 +317,42 @@ public class SunshineServer {
         long downTime = SystemClock.uptimeMillis();
         long eventTime = SystemClock.uptimeMillis();
         
-        // 计算活跃的触摸点数量
-        int pointerCount = 0;
-        for (PointerStatus status : pointers) {
-            if (status.isDown) pointerCount++;
-        }
-        
-        if (pointerCount == 0) return;
-        
-        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
-        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
+
+        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointers.size()];
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointers.size()];
         
         int index = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            PointerStatus status = pointers.get(i);
-            if (status.isDown) {
-                properties[index] = new MotionEvent.PointerProperties();
-                properties[index].id = i;
-                properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
-                
-                coords[index] = new MotionEvent.PointerCoords();
-                coords[index].x = status.x;
-                coords[index].y = status.y;
-                coords[index].pressure = 1.0f;
-                index++;
-                
-                // 重置状态
-                status.isDown = false;
-            }
+        for (Integer k : pointers.keySet()) {
+            PointerStatus status = pointers.get(k);
+            properties[index] = new MotionEvent.PointerProperties();
+            properties[index].id = k;
+            properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
+
+            coords[index] = new MotionEvent.PointerCoords();
+            coords[index].x = status.x;
+            coords[index].y = status.y;
+            coords[index].pressure = 1.0f;
+            index++;
         }
         
         MotionEvent event = MotionEvent.obtain(
             downTime,
             eventTime,
             android.view.MotionEvent.ACTION_CANCEL,
-            pointerCount,
+            pointers.size(),
             properties,
             coords,
             0, 0, 1.0f, 1.0f, 0, 0,
             InputDevice.SOURCE_TOUCHSCREEN,
             0
         );
+        pointers.clear();
         
         if (State.mirrorVirtualDisplay != null) {
             MotionEventHidden motionEventHidden = Refine.unsafeCast(event);
             motionEventHidden.setDisplayId(State.mirrorVirtualDisplay.getDisplay().getDisplayId());
             inputManager.injectInputEvent(event, 0);
+            android.util.Log.d("SunshineServer", "inject cancel: " + event);
         }
     }
 
@@ -369,36 +360,28 @@ public class SunshineServer {
         long downTime = SystemClock.uptimeMillis();
         long eventTime = SystemClock.uptimeMillis();
         
-        // 计算活跃的触摸点数量
-        int pointerCount = 0;
-        for (PointerStatus status : pointers) {
-            if (status.isDown) pointerCount++;
-        }
-        
-        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
-        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
+        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointers.size()];
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointers.size()];
         
         int index = 0;
-        for (int i = 0; i < pointers.size(); i++) {
-            PointerStatus status = pointers.get(i);
-            if (status.isDown) {
-                properties[index] = new MotionEvent.PointerProperties();
-                properties[index].id = i;
-                properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
-                
-                coords[index] = new MotionEvent.PointerCoords();
-                coords[index].x = status.x;
-                coords[index].y = status.y;
-                coords[index].pressure = 1.0f;
-                index++;
-            }
+        for (Integer k : pointers.keySet()) {
+            PointerStatus status = pointers.get(k);
+            properties[index] = new MotionEvent.PointerProperties();
+            properties[index].id = k;
+            properties[index].toolType = MotionEvent.TOOL_TYPE_FINGER;
+
+            coords[index] = new MotionEvent.PointerCoords();
+            coords[index].x = status.x;
+            coords[index].y = status.y;
+            coords[index].pressure = 1.0f;
+            index++;
         }
         
         MotionEvent event = MotionEvent.obtain(
             downTime,
             eventTime,
             android.view.MotionEvent.ACTION_MOVE,
-            pointerCount,
+            pointers.size(),
             properties,
             coords,
             0, 0, 1.0f, 1.0f, 0, 0,
@@ -410,7 +393,9 @@ public class SunshineServer {
             MotionEventHidden motionEventHidden = Refine.unsafeCast(event);
             motionEventHidden.setDisplayId(State.mirrorVirtualDisplay.getDisplay().getDisplayId());
             inputManager.injectInputEvent(event, 0);
+            android.util.Log.d("SunshineServer", "inject move: " + event);
         }
+
     }
 
 }
