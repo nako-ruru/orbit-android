a5a6975adba46a7c57236e36b4708f170724f881
Tao Wen <taowen@gmail.com>
2025-03-27 23:23:23 +0800

    update

---
+++ app-mirror/src/main/java/com/connect_screen/mirror/shizuku/FakeContext.java
@@ -0,0 +1,62 @@
+package com.connect_screen.mirror.shizuku;
+
+
+import android.annotation.TargetApi;
+import android.content.AttributionSource;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Binder;
+import android.os.Process;
+
+import com.connect_screen.mirror.job.AndroidVersions;
+
+public final class FakeContext extends ContextWrapper {
+
+    public static final String PACKAGE_NAME = "com.android.shell";
+    public static final int ROOT_UID = 0; // Like android.os.Process.ROOT_UID, but before API 29
+
+    private static final FakeContext INSTANCE = new FakeContext();
+
+    public static FakeContext get() {
+        return INSTANCE;
+    }
+
+    private FakeContext() {
+        super(Workarounds.getSystemContext());
+    }
+
+    @Override
+    public String getPackageName() {
+        return PACKAGE_NAME;
+    }
+
+    @Override
+    public String getOpPackageName() {
+        return PACKAGE_NAME;
+    }
+
+    @TargetApi(AndroidVersions.API_31_ANDROID_12)
+    @Override
+    public AttributionSource getAttributionSource() {
+        AttributionSource.Builder builder = new AttributionSource.Builder(Process.SHELL_UID);
+        builder.setPackageName(PACKAGE_NAME);
+        return builder.build();
+    }
+
+    // @Override to be added on SDK upgrade for Android 14
+    @SuppressWarnings("unused")
+    public int getDeviceId() {
+        return 0;
+    }
+
+    @Override
+    public Context getApplicationContext() {
+        return this;
+    }
+
+    @Override
+    public ContentResolver getContentResolver() {
+        return null;
+    }
+}
