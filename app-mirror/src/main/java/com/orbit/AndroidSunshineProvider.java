dbce91dd46c799f36f2403e21bd7ae074775cbd0
bright <nako_ruru@sina.com>
2025-12-29 15:47:49 +0800

    sunshine ok

--- app-mirror/src/main/java/com/orbit/AndroidSunshineProvider.java
+++ app-mirror/src/main/java/com/orbit/AndroidSunshineProvider.java
@@ -1,11 +1,7 @@
 package com.orbit;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
 
-import com.connect_screen.mirror.State;
-import com.connect_screen.mirror.SunshineService;
 import com.connect_screen.mirror.job.SunshineServer;
 
 import aar.SunshineProvider;
@@ -24,32 +20,13 @@ public class AndroidSunshineProvider implements SunshineProvider {
 
     @Override
     public boolean inputPin(String pin, String name) throws Exception {
-//        SunshineServer.onPinRequested();
+        SunshineServer.submitPin(pin);
         return true;
     }
-
-    // 静态存储授权后的数据，只要进程不死就一直有效
-    public static Intent cachedData = null;
 
     /**
      * Golang 调用此方法: 一键启动投屏
      */
     public static void requestStart(Context context) {
-        /*
-        Intent intent = new Intent(context, ProjectionPermissionActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // 确保从非 Activity 环境也能启动
-        context.startActivity(intent);
-         */
-    }
-
-    // 内部统一启动 Service 方法
-    public static void launchService(Context context, Intent data) {
-        Intent serviceIntent = new Intent(context, SunshineService.class);
-        serviceIntent.putExtra("data", data);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(serviceIntent);
-        } else {
-            context.startService(serviceIntent);
-        }
     }
 }
