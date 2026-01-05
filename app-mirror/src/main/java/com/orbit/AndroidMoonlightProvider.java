6f1edfc9d0f1f6097482d91cac92f136c453be23
bright <nako_ruru@sina.com>
2026-01-05 16:20:29 +0800

    ipv6

--- app-mirror/src/main/java/com/orbit/AndroidMoonlightProvider.java
+++ app-mirror/src/main/java/com/orbit/AndroidMoonlightProvider.java
@@ -2,8 +2,6 @@ package com.orbit;
 
 import android.app.Activity;
 
-import androidx.annotation.NonNull;
-
 import aar.MoonlightProvider;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -20,8 +18,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
-import java.security.cert.X509Certificate;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,22 +34,14 @@ public class AndroidMoonlightProvider implements MoonlightProvider {
 
     @Override
     public void connectRemote(String sunshineAddr, String localIp) throws Exception {
-        b();
-        NvHTTP httpConn = createNvHTTP(sunshineAddr, localIp);
+        ComputerDetails.AddressTuple addressTuple = createAddressTuple(sunshineAddr);
+        NvHTTP httpConn = createNvHTTP(addressTuple, localIp);
         NvApp app = httpConn.getAppByName("Desktop");
-//        a(httpConn, PlatformBinding.getCryptoProvider(context).getPemEncodedClientCertificate());
         ComputerDetails computer = httpConn.getComputerDetails(true);
         ComputerDetails persistent = new ComputerDatabaseManager(context).getComputerByUUID(computer.uuid);
         computer.serverCert = persistent.serverCert;
-        /*
-        computer.activeAddress = new ComputerDetails.AddressTuple("192.168.3.19", 47989);
-        computer.manualAddress = new ComputerDetails.AddressTuple("192.168.3.19", 47989);
-        computer.ipv6Address = new ComputerDetails.AddressTuple("[2408:8207:78b2:9830:7407:daff:ebb9:a8d3]", 47989);
-        computer.macAddress = "c0:18:85:20:48:ac";
-        computer.pairState = PairingManager.PairState.PAIRED;
-         */
-        computer.activeAddress = new ComputerDetails.AddressTuple("192.168.3.19", 47989);
-        doStart(app, computer);
+        computer.activeAddress = addressTuple;
+        doStart(app, computer, localIp);
     }
 
     @Override
@@ -97,27 +85,31 @@ public class AndroidMoonlightProvider implements MoonlightProvider {
 
     }
 
-    @NonNull
     private NvHTTP createNvHTTP(String sunshineAddr, String localIp) throws IOException {
-        if(true) {
-            return new NvHTTP(new ComputerDetails.AddressTuple("192.168.3.19", 47989), 47984, "ignored", null, PlatformBinding.getCryptoProvider(context));
-        }
-        int port;
-        String sunshineIp;
-        Matcher matcher = Pattern.compile(":\\d+$", 0).matcher(sunshineAddr);
-        if(matcher.find()) {
-            port = Integer.parseInt(matcher.group(0));
-            sunshineIp = sunshineAddr.substring(0, matcher.start());
-        } else {
-            port = 47989;
-            sunshineIp = sunshineAddr;
-        }
-        ComputerDetails.AddressTuple addressTuple = new ComputerDetails.AddressTuple(sunshineIp, port);
-        SocketFactory socketFactory = new BoundSocketFactory(InetAddress.getByName(localIp));
-        return NvHTTPFactory.create(addressTuple, port - 5, "ignored", null, PlatformBinding.getCryptoProvider(context), socketFactory);
+        return createNvHTTP(createAddressTuple(sunshineAddr), localIp);
     }
 
-    private void doStart(NvApp app, ComputerDetails computer) {
+    private NvHTTP createNvHTTP(ComputerDetails.AddressTuple addressTuple, String localIp) throws IOException {
+        SocketFactory socketFactory = new BoundSocketFactory(InetAddress.getByName(localIp));
+        return new NvHTTP(addressTuple, addressTuple.port - 5, "ignored", null, PlatformBinding.getCryptoProvider(context), socketFactory);
+    }
+
+    private ComputerDetails.AddressTuple createAddressTuple(String addr) {
+        int port;
+        String sunshineIp;
+        Matcher matcher = Pattern.compile(":\\d+$", 0).matcher(addr);
+        if(matcher.find()) {
+            port = Integer.parseInt(matcher.group(0));
+            sunshineIp = addr.substring(0, matcher.start());
+        } else {
+            port = 47989;
+            sunshineIp = addr;
+        }
+        ComputerDetails.AddressTuple addressTuple = new ComputerDetails.AddressTuple(sunshineIp, port);
+        return addressTuple;
+    }
+
+    private void doStart(NvApp app, ComputerDetails computer, String srcIP) {
         ComputerManagerService.ComputerManagerBinder mockBinder = new ComputerManagerService().new ComputerManagerBinder() {
             public String getUniqueId() {
                 if (managerBinder == null) {
@@ -126,7 +118,7 @@ public class AndroidMoonlightProvider implements MoonlightProvider {
                 return managerBinder.getUniqueId();
             }
         };
-        ServerHelper.doStart(context, app, computer, mockBinder);
+        ServerHelper.doStart(context, app, computer, srcIP, mockBinder);
     }
 
     private boolean paired(NvHTTP httpConn) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, IOException, InvocationTargetException {
@@ -146,61 +138,5 @@ public class AndroidMoonlightProvider implements MoonlightProvider {
             return pairedState.equals("1");
         }
         return false;
-    }
-
-    private void b() {
-        List<ComputerDetails> allComputers = new ComputerDatabaseManager(this.context).getAllComputers();
-    }
-
-    private void a(NvHTTP http, byte[] pemCertBytes) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
-        PairingManager pm = http.getPairingManager();
-
-        Method generateRandomBytes = PairingManager.class.getDeclaredMethod("generateRandomBytes", int.class);
-        if(!generateRandomBytes.isAccessible()) {
-            generateRandomBytes.setAccessible(true);
-        }
-        // Generate a salt for hashing the PIN
-        byte[] salt = (byte[]) generateRandomBytes.invoke(pm,16);
-
-        Method bytesToHex = NvHTTP.class.getDeclaredMethod("bytesToHex", byte[].class);
-        if(!bytesToHex.isAccessible()) {
-            bytesToHex.setAccessible(true);
-        }
-        Method executePairingCommand = NvHTTP.class.getDeclaredMethod("executePairingCommand", String.class, boolean.class);
-        if(!executePairingCommand.isAccessible()) {
-            executePairingCommand.setAccessible(true);
-        }
-        // Send the salt and get the server cert. This doesn't have a read timeout
-        // because the user must enter the PIN before the server responds
-        String getCert = (String) executePairingCommand.invoke(http, "phrase=getservercert&salt="+
-                        bytesToHex.invoke(null, salt)+"&clientcert="+bytesToHex.invoke(null, pemCertBytes),
-                false);
-        Method getXmlString = NvHTTP.class.getDeclaredMethod("getXmlString", String.class, String.class, boolean.class);
-        if(!getXmlString.isAccessible()) {
-            getXmlString.setAccessible(true);
-        }
-        if (!getXmlString.invoke(http, getCert, "paired", true).equals("1")) {
-            throw new IllegalStateException(PairingManager.PairState.FAILED.name());
-        }
-
-        Method extractPlainCert = PairingManager.class.getDeclaredMethod("extractPlainCert", String.class);
-        if(!extractPlainCert.isAccessible()) {
-            extractPlainCert.setAccessible(true);
-        }
-        // Save this cert for retrieval later
-        X509Certificate serverCert = (X509Certificate) extractPlainCert.invoke(pm, getCert);
-        if (serverCert == null) {
-            // Attempting to pair while another device is pairing will cause GFE
-            // to give an empty cert in the response.
-            http.unpair();
-            throw new IllegalStateException(PairingManager.PairState.ALREADY_IN_PROGRESS.name());
-        }
-
-        Method setServerCert = NvHTTP.class.getDeclaredMethod("setServerCert", X509Certificate.class);
-        if(!setServerCert.isAccessible()) {
-            setServerCert.setAccessible(true);
-        }
-        // Require this cert for TLS to this host
-        setServerCert.invoke(http, serverCert);
     }
 }
